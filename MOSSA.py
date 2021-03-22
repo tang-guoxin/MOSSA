765b8112dab339b0b6ef94ea14fd33779b734d7e
T-XiaoMeng <tgx126sicnu@126.com>
2021-03-22 12:45:27 +0800

    first commit

---
+++ MOSSA.py
@@ -0,0 +1,11 @@
+# user/bin/env python
+# coding = 'utf-8'
+
+import numpy as np
+
+class creat():
+    def __init__(self):
+        pass
+
+    def fit(self, strs):
+        print(strs)
---
+++ main.py
@@ -0,0 +1,7 @@
+# coding = 'utf-8'
+
+import MOSSA
+
+mode = MOSSA.creat()
+
+mode.fit(1212)
