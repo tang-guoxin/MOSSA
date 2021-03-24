6a486824e8204ac0801004a4f80cb0afadfdf866
T-XiaoMeng <tgx126sicnu@126.com>
2021-03-24 08:56:31 +0800

    'updata-at-8:56'

--- MOSSA.py
+++ MOSSA.py
@@ -5,6 +5,7 @@
 fun:    多目标目标函数，返回值应是一个多维矩阵
 nVar:   种群数目
 dim:    维度
+num:    函数个数
 lb:     搜索下边界
 ub:     搜索上边界
 vlb:    速度下边界
@@ -20,12 +21,13 @@ import numpy as np
 
 
 class creat():
-    def __init__(self, fun, nVar, dim, lb, ub, vlb, vub, prod=0.2, pred=0.1, max_iter=100, eps=1e-6, w=None):
+    def __init__(self, fun, nVar, dim, num, lb, ub, vlb, vub, prod=0.2, pred=0.1, max_iter=100, eps=1e-6, w=None, arc=0.2):
         if w is None:
             w = [0.1, 0.9]
         self.fun = fun
         self.nVar = nVar
         self.dim = dim
+        self.num = num
         self.lb = lb
         self.ub = ub
         self.vlb = vlb
@@ -35,9 +37,21 @@ class creat():
         self.max_iter = max_iter
         self.eps = eps
         self.w = w
-
+        # 初始化种群
         self.pop = np.random.uniform(lb, ub, (nVar, dim))
+        # 初始化速度
         self.vel = np.random.uniform(vlb, vub, (nVar, dim))
+        # 初始化存档
+        # self.rep = np.ones((np.floor(arc*nVar), dim))
+        self.rep_num = np.floor(arc*nVar)
+        pass
+
+    def paretoDominant(self, pop, rep):
+        n = len(rep)
+        for i in range(self.nVar):
+            for j in range(n):
+                for k in range(self.num):
+                    if pop[i, k] > pop[j][k]
         pass
 
     def fit(self, strs):
--- ObjFunc.py
+++ ObjFunc.py
@@ -4,11 +4,14 @@ test function
 '''
 import numpy as np
 
+
 def f1(x):
-    return -x[:, 0]**2 + x[:, 1]
+    return -x[:, 0] ** 2 + x[:, 1]
+
 
 def f2(x):
     return 0.5 * x[:, 0] + x[:, 1] + 1
+
 
 def fun(x):
     n = x.shape[0]
@@ -16,4 +19,3 @@ def fun(x):
     ret[:, 0] = f1(x)
     ret[:, 1] = f2(x)
     return ret
-
