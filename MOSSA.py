2384a520951d35b147aaa4abf404769682ec0289
T-XiaoMeng <tgx126sicnu@126.com>
2021-03-24 21:11:13 +0800

    'updata-at-107'

--- MOSSA.py
+++ MOSSA.py
@@ -1,27 +1,34 @@
 # user/bin/env python3
 # coding = 'utf-8'
 ####################################
-'''
-fun:    多目标目标函数，返回值应是一个多维矩阵
-nVar:   种群数目
-dim:    维度
-num:    函数个数
-lb:     搜索下边界
-ub:     搜索上边界
-vlb:    速度下边界
-vub:    速度上边界
-prod:   生产者比例
-pred:   察觉危险者比例
-max_iter: 最大迭代次数
-eps:    误差上限
-w:      动态权重区间
+Version = '''
+---------------------------------------
+create by T-XiaoMeng at 3/24/2021 18:32
+v 1.0.0
+---------------------------------------
+fun:        多目标目标函数，返回值应是一个多维矩阵
+nVar:       种群数目
+dim:        维度
+num:        函数个数
+lb:         搜索下边界
+ub:         搜索上边界
+vlb:        速度下边界
+vub:        速度上边界
+prod:       生产者比例
+pred:       察觉危险者比例
+max_iter:   最大迭代次数
+eps:        误差上限
+w:          动态权重区间
+gdn:        网格细度
+---------------------------------------
+rep 是一个列表，每个元素也是一个列表，第一个值为位置坐标，第二个值为多目标函数的适应度值，长度是一个定值
 '''
 ####################################
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class creat():
-    def __init__(self, fun, nVar, dim, num, lb, ub, vlb, vub, prod=0.2, pred=0.1, max_iter=100, eps=1e-6, w=None, arc=0.2):
+    def __init__(self, fun, nVar, dim, num, lb, ub, vlb, vub, prod=0.2, pred=0.1, max_iter=100, eps=1e-6, w=None, arc=0.2, gdn=10):
         if w is None:
             w = [0.1, 0.9]
         self.fun = fun
@@ -37,6 +44,7 @@ class creat():
         self.max_iter = max_iter
         self.eps = eps
         self.w = w
+        self.gdn = gdn
         # 初始化种群
         self.pop = np.random.uniform(lb, ub, (nVar, dim))
         # 初始化速度
@@ -46,17 +54,55 @@ class creat():
         self.rep_num = np.floor(arc*nVar)
         pass
 
-    def paretoDominant(self, pop, rep):
-        n = len(rep)
-        for i in range(self.nVar):
-            for j in range(n):
-                for k in range(self.num):
-                    if pop[i, k] > pop[j][k]
+
+    # 获取 rep 边界
+    def getBound(self, rep):
+        Max, Min = -0xffffff, 0xffffff
+        ary = []
+        for ls in rep:
+            ary.append(ls[1])
+        arys = np.array(ary)
+        Max = np.max(arys, 0) + 0.1
+        Min = np.min(arys, 0) - 0.1
+        return Max, Min
+
+    # 自适应网格优化: grid opt
+    def gridOpt(self, rep):
+        [Max, Min] = self.getBound(rep)
+
+        print([Max, Min])
         pass
 
-    def fit(self, strs):
-        print(strs)
 
+    # 初始化 rep 档案集: pareto 支配解
+    def initRep(self, fval, pop):
+        rep = []
+        for i in range(self.nVar):
+            flag = True
+            for j in range(i, self.nVar):
+                if np.all(fval[i, :]) > np.all(fval[j, :]):
+                    flag = False
+                    break
+            if flag == True:
+                rep.append([pop[i, :], fval[i, :]])
+        if len(rep) > self.rep_num:
+            rep = self.gridOpt(rep) # 是否可以等循环结束后再一次性网格优化?
+        return rep
+        pass
 
-x = np.random.uniform(1, 3, (2, 2))
-print(x)
+    def paretoDominant(self, pop, rep):
+        pass
+
+    def train(self, show=False):
+        fval = self.fun(self.pop)
+        rep = self.initRep(fval, self.pop)
+
+        pass
+
+import ObjFunc
+fun = ObjFunc.fun
+#%% -------------------------------------------
+mode = creat(fun, 10, 2, 2, 0, 1, -1, 1)
+
+mode.train()
+
